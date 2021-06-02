8567e6e3825fea1af7cd75e736b8718413d873fc
Balabukh Nick <nick_bron@ukr.net>
2021-06-02 10:47:36 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
 const navEl = document.querySelectorAll('.item');
-console.log(navEl);
+console.log("В списке", navEl.length, "элемента");
+
+const nav = document.querySelector('.item.h2');
+console.log("В списке", nav, "элемента");
+
+console.log("Категория:");
+
+console.log("Количество элементов:");
