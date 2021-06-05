3b8160d3ae1ffe2682c90b962ae34fcda0bbeef0
nickbron <77615583+nickbron@users.noreply.github.com>
2021-06-05 21:30:59 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -10,13 +10,23 @@ const ingredients = [
 
 const navEl = document.querySelector("#ingredients");
 
-const elements = ingredients.map(elem => {
-   const navItemEl = document.createElement("li");
-   navItemEl.textContent = elem ;
-  console.log(navItemEl);
-  return navItemEl;
-});
+// const elements = ingredients.map(elem => {
+//    const navItemEl = document.createElement("li");
+//    navItemEl.textContent = elem ;
+//   console.log(navItemEl);
+//   return navItemEl;
+// });
 
+const makeIngredients = elem => {
+  return elem.map(elem => {
+    const navItemEl = document.createElement("li");
+    navItemEl.textContent = elem;
+    console.log(navItemEl);
+    return navItemEl;
+  });
+};
+
+const elements = makeIngredients(ingredients);
 navEl.append(...elements);
 
 
