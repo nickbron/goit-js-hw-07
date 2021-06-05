4a32861c27ea490cf3682339b8902e48cfa4e325
nickbron <77615583+nickbron@users.noreply.github.com>
2021-06-05 12:44:01 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,33 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const navEl = document.querySelector("#ingredients");
+
+
+
+// ingredients.forEach((elem) => {
+ 
+//   const navItemEl = document.createElement("li");
+//    navItemEl.textContent = elem ;
+//    console.log(navItemEl);
+  
+//   navEl.appendChild(navItemEl);
+// });
+
+const elements = ingredients.map(elem => {
+   const navItemEl = document.createElement("li");
+   navItemEl.textContent = elem ;
+  console.log(navItemEl);
+  return navItemEl;
+});
+
+navEl.append(...elements);
+
+
+
+
+  
+
+
+
