ad04234c76e854545f66408c53d91bba833d9a2b
nickbron <77615583+nickbron@users.noreply.github.com>
2021-06-10 18:01:54 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,23 +4,17 @@ const expectedLength = Number(input.dataset.length);
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur() {
-    const isValid = expectedLength === input.value.length;
-    if (isValid) {
-        setValidStateClasses();
-    } else {
-        setInvalidStateClasses();
-    }
+const isValid = expectedLength === input.value.length;
+if (isValid) {
+setClasses("valid", "invalid");
+} else {
+setClasses("invalid", "valid");
+}
 }
 
-function setValidStateClasses() {
-    input.classList.remove('invalid');
-    input.classList.add('valid');
-}
-
-function setInvalidStateClasses() {
-    input.classList.remove('valid');
-    input.classList.add('invalid');
+function setClasses(addClass, remClass) {
+    input.classList.add(addClass);
+    input.classList.remove(remClass);
 }
 
 // function addClass(method, value) {input.classList[method](value); }
-
