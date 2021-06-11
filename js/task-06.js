1f138c80ac42eff00525a405ac48b7677590769b
Balabukh Nick <nick_bron@ukr.net>
2021-06-11 12:00:36 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const input = document.getElementById("validation-input");
 const expectedLength = Number(input.dataset.length);
 
@@ -19,28 +18,3 @@ function setClasses(addClass, remClass) {
 }
 
 // function addClass(method, value) {input.classList[method](value); }
-=======
-const refs = {
-    input:document.getElementById("validation-input"),
-    // nameLabel: document.querySelector("#name-output"),
-};
-
-refs.input.addEventListener("blur", onInputBlur);
-
-function onInputBlur(event) {
-    if (Number(refs.input.dataset.length) === event.currentTarget.value.length) {
-        //  refs.input.classList.remove("invalid");
-        // refs.input.classList.add("valid");
-        classAdd(remove,valid);
-    }
-    else {
-        refs.input.classList.add("invalid");
-            // refs.input.classList.remove("valid");
-        }
-}
-
-function classAdd (classValue,value)
-{
-    refs.input.classList.classValue("value");
-}
->>>>>>> Stashed changes
